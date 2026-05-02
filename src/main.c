4cb2a27694edcfb9151decfe3dd6770da5867086
Radosław Bąkowski <radoslaw.bakowski@univio.com>
2026-05-02 15:18:26 +0200

    KV_GET implementation

--- src/main.c
+++ src/main.c
@@ -8,11 +8,13 @@ int main() {
 
 	kv_put(table, "hehe", "haha");
 	kv_put(table, "hehe", "hoho");
-	kv_put(table, "lala", "hoho");
-	for (int i = 0; i < table->capacity; i++) {
-		if(table->entries[i].key) {
-			printf("[%d] %s: %s\n", i, table->entries[i].key, table->entries[i].value);
-		}
-	}
+	kv_put(table, "lala", "baba");
+
+	char *val = kv_get(table, "hehe");
+	char *val2 = kv_get(table, "lala");
+	char *val3 = kv_get(table, "this_does_not_exist");
+
+	printf("%s %s %s\n", val, val2, val3);
+
 	free(table);
 }
